5af5e306a02ec0c7c9b1d938e7570fa97ae73d43
navyagolla910 <106624303+navyagolla910@users.noreply.github.com>
2022-06-16 02:35:27 +0530

    Create main.tf

---
+++ ec2-provision/main.tf
@@ -0,0 +1,33 @@
+# main.tf
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-06eecef118bbf9259"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+# variables.tf
+
+variable "instance_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "SampleAppServerInstance"
+}
